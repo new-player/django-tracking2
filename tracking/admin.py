4b57e869ccb53b9a867526719cf86e8980830bb2
Sid <siddharthjhanwar@yahoo.in>
2018-06-18 15:45:28 +0530

    Updated search for django admin site

--- tracking/admin.py
+++ tracking/admin.py
@@ -9,7 +9,7 @@ class VisitorAdmin(admin.ModelAdmin):
     list_display = ('session_key', 'user', 'start_time', 'session_over',
         'pretty_time_on_site', 'ip_address', 'user_agent')
     list_filter = ('user',)
-    search_fields = ('session_key', 'user__email', 'user__username', 'user_agent',)
+    search_fields = ('session_key', 'user__email', 'user__username', 'user_agent', )
 
     readonly_fields=('user_agent', 'ip_address', 'start_time', 'expiry_age',\
                      'expiry_time', 'time_on_site', 'end_time', 'geoip_data', )
@@ -31,7 +31,11 @@ class VisitorAdmin(admin.ModelAdmin):
             list_display += ('geoip_data',)
         return list_display
         
-
+    def get_search_fields(self, request):
+        search_fields = super(VisitorAdmin, self).get_search_fields(request)
+        if TRACK_USING_GEOIP:
+            search_fields += ('geoip_data',)
+        return search_fields
 
 admin.site.register(Visitor, VisitorAdmin)
 
@@ -39,7 +43,7 @@ admin.site.register(Visitor, VisitorAdmin)
 class PageviewAdmin(admin.ModelAdmin):
     date_hierarchy = 'view_time'
     raw_id_fields = ['visitor',]
-    search_fields = ('visitor__session_key', 'visitor__user__email', 'url', 'referer', 'query_string', 'method',)
+    search_fields = ('visitor__session_key', 'visitor__user__email', 'visitor__user__username', 'url', 'referer', 'query_string', 'method',)
     list_display = ('visitor', 'url', 'view_time', 'query_string','referer',)
     readonly_fields = ('query_string', 'referer', 'url', )
 
